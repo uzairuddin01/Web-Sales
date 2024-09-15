f0c5864c7d011e82a6b38cea25c3c3e22921a10e
md uzair uddin <mduzairuddin01@gmail.com>
2024-09-15 10:17:55 +0530

    Initial Commit

---
+++ Web-Sales/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web-Sales
+//
+//  Created by Sainath Reddy Sadhari on 15/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
